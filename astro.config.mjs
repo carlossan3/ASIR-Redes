bc09e5ea6b4f3bd905514ad98642d533a40f1bd3
Carlos <carlos.sanchez3@murciaeduca.es>
2026-09-17 18:42:57 +0200

    v14

--- astro.config.mjs
+++ astro.config.mjs
@@ -35,10 +35,41 @@ export default defineConfig({
 sidebar: [
         {
           label: 'U1 · Fundamentos de redes',
-          translations: { en: 'U1 · Network fundamentals' },
+          translations: { en: 'U1 · Network Fundamentals' },
           collapsed: false,
           items: [
-            { autogenerate: { directory: 'U01' } },
+            // Entradas en Español
+            {
+              label: '1 · Comunicación y red de datos',
+              link: '/ASIR-Redes/es/u01/u0101comunicacion_y_red_de_datos_es/',
+              attrs: { class: 'es-only' },
+            },
+            {
+              label: '2 · Componentes y primera representación',
+              link: '/ASIR-Redes/es/u01/u0102componentes_y_primera_representacion_es/',
+              attrs: { class: 'es-only' },
+            },
+            {
+              label: '3 · Tipos de red y arquitecturas de servicio',
+              link: '/ASIR-Redes/es/u01/u0103tipos_de_red_y_arquitecturas_de_servicio_es/',
+              attrs: { class: 'es-only' },
+            },
+            // Entradas en Inglés
+            {
+              label: '1 · Communication and data networks',
+              link: '/ASIR-Redes/en/u01/u0101communication_and_data_networks_en/',
+              attrs: { class: 'en-only' },
+            },
+            {
+              label: '2 · Components and first network representation',
+              link: '/ASIR-Redes/en/u01/u0102components_and_first_network_representation_en/',
+              attrs: { class: 'en-only' },
+            },
+            {
+              label: '3 · Network types and service architectures',
+              link: '/ASIR-Redes/en/u01/u0103network_types_and_service_architectures_en/',
+              attrs: { class: 'en-only' },
+            },
           ],
         },
       ],
