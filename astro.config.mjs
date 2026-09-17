71c67d438418b4e5838c7bc47ba3edb79165d6c5
Carlos <carlos.sanchez3@murciaeduca.es>
2026-09-17 18:51:19 +0200

    v17

--- astro.config.mjs
+++ astro.config.mjs
@@ -41,34 +41,34 @@ export default defineConfig({
             // ESPAÑOL
             {
               label: '1 · Comunicación y red de datos',
-              link: '/es/U01/U01.01.Comunicacion_y_red_de_datos_ES/',
+              link: 'U01/U01.01.Comunicacion_y_red_de_datos_ES/',
               attrs: { class: 'nav-es' },
             },
             {
               label: '2 · Componentes y primera representación',
-              link: '/es/U01/U01.02.Componentes_y_primera_representacion_ES/',
+              link: 'U01/U01.02.Componentes_y_primera_representacion_ES/',
               attrs: { class: 'nav-es' },
             },
             {
               label: '3 · Tipos de red y arquitecturas de servicio',
-              link: '/es/U01/U01.03.Tipos_de_red_y_arquitecturas_de_servicio_ES/',
+              link: 'U01/U01.03.Tipos_de_red_y_arquitecturas_de_servicio_ES/',
               attrs: { class: 'nav-es' },
             },
 
             // INGLÉS
             {
               label: '1 · Communication and data networks',
-              link: '/en/U01/U01.01.Communication_and_data_networks_EN/',
+              link: '../en/U01/U01.01.Communication_and_data_networks_EN/',
               attrs: { class: 'nav-en' },
             },
             {
               label: '2 · Components and first network representation',
-              link: '/en/U01/U01.02.Components_and_first_network_representation_EN/',
+              link: '../en/U01/U01.02.Components_and_first_network_representation_EN/',
               attrs: { class: 'nav-en' },
             },
             {
               label: '3 · Network types and service architectures',
-              link: '/en/U01/U01.03.Network_types_and_service_architectures_EN/',
+              link: '../en/U01/U01.03.Network_types_and_service_architectures_EN/',
               attrs: { class: 'nav-en' },
             },
           ],
