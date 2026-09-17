224b24caebbb26c3b4d54c6b5ac53caf7dab45de
Carlos <carlos.sanchez3@murciaeduca.es>
2026-09-17 18:54:23 +0200

    v18

--- astro.config.mjs
+++ astro.config.mjs
@@ -32,53 +32,11 @@ export default defineConfig({
         },
       },
 
-	  sidebar: [
-        {
-          label: 'U1 · Fundamentos de redes',
-          translations: { en: 'U1 · Network Fundamentals' },
-          collapsed: false,
-          items: [
-            // ESPAÑOL
-            {
-              label: '1 · Comunicación y red de datos',
-              link: 'U01/U01.01.Comunicacion_y_red_de_datos_ES/',
-              attrs: { class: 'nav-es' },
-            },
-            {
-              label: '2 · Componentes y primera representación',
-              link: 'U01/U01.02.Componentes_y_primera_representacion_ES/',
-              attrs: { class: 'nav-es' },
-            },
-            {
-              label: '3 · Tipos de red y arquitecturas de servicio',
-              link: 'U01/U01.03.Tipos_de_red_y_arquitecturas_de_servicio_ES/',
-              attrs: { class: 'nav-es' },
-            },
-
-            // INGLÉS
-            {
-              label: '1 · Communication and data networks',
-              link: '../en/U01/U01.01.Communication_and_data_networks_EN/',
-              attrs: { class: 'nav-en' },
-            },
-            {
-              label: '2 · Components and first network representation',
-              link: '../en/U01/U01.02.Components_and_first_network_representation_EN/',
-              attrs: { class: 'nav-en' },
-            },
-            {
-              label: '3 · Network types and service architectures',
-              link: '../en/U01/U01.03.Network_types_and_service_architectures_EN/',
-              attrs: { class: 'nav-en' },
-            },
-          ],
-        },
-      ],
-
       tableOfContents: false,
 
       components: {
         ThemeProvider: './src/components/Accesibilidad.astro',
+        Sidebar: './src/components/CustomSidebar.astro',
       },
 
       social: [
