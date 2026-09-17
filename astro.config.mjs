85ca0374116a9004173a8e9b4f82d4a776b6eed0
Carlos <carlos.sanchez3@murciaeduca.es>
2026-09-17 18:17:52 +0200

    astro.config v3

--- astro.config.mjs
+++ astro.config.mjs
@@ -34,39 +34,34 @@ export default defineConfig({
       },
 
       sidebar: [
-        {
-          label: 'Inicio',
-          translations: { en: 'Home' },
-          slug: '/es/',
-        },
-        {
-          label: 'U1 · Fundamentos de redes',
-          translations: { en: 'U1 · Network fundamentals' },
-          collapsed: false,
-          items: [
-            {
-              slug: 'es/U01-Fundamentos',
-              label: 'Índice de la unidad',
-              translations: { en: 'Unit index' },
-            },
-            {
-              slug: 'es/U01-Fundamentos/U01.01.Comunicacion_y_red_de_datos_ES',
-              label: '1 · Comunicación y red de datos',
-              translations: { en: '1 · Communication and data networks' },
-            },
-            {
-              slug: 'es/U01-Fundamentos/U01.02.Componentes_y_primera_representacion_ES',
-              label: '2 · Componentes y primera representación',
-              translations: { en: '2 · Components and first network representation' },
-            },
-            {
-              slug: 'es/U01-Fundamentos/U01.03.Tipos_de_red_y_arquitecturas_de_servicio_ES',
-              label: '3 · Tipos de red y arquitecturas de servicio',
-              translations: { en: '3 · Network types and service architectures' },
-            },
-          ],
-        },
-      ],
+  {
+    label: 'U1 · Fundamentos de redes',
+    translations: { en: 'U1 · Network fundamentals' },
+    collapsed: false,
+    items: [
+      {
+        slug: 'es/U01-Fundamentos',
+        label: 'Índice de la unidad',
+        translations: { en: 'Unit index' },
+      },
+      {
+        slug: 'es/U01-Fundamentos/U01.01.Comunicacion_y_red_de_datos_ES',
+        label: '1 · Comunicación y red de datos',
+        translations: { en: '1 · Communication and data networks' },
+      },
+      {
+        slug: 'es/U01-Fundamentos/U01.02.Componentes_y_primera_representacion_ES',
+        label: '2 · Componentes y primera representación',
+        translations: { en: '2 · Components and first network representation' },
+      },
+      {
+        slug: 'es/U01-Fundamentos/U01.03.Tipos_de_red_y_arquitecturas_de_servicio_ES',
+        label: '3 · Tipos de red y arquitecturas de servicio',
+        translations: { en: '3 · Network types and service architectures' },
+      },
+    ],
+  },
+],
 
       tableOfContents: false,
 
