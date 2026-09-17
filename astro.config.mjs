1e958d84ffc1e5841c7d6307a06820481fe60d8c
Carlos <carlos.sanchez3@murciaeduca.es>
2026-09-17 18:34:31 +0200

    Otra vez cambios estructura

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,7 +14,6 @@ export default defineConfig({
   integrations: [
     starlight({
       title: 'Redes',
-
       description: 'Apuntes de ASIR-Redes',
 
       customCss: [
@@ -33,19 +32,44 @@ export default defineConfig({
         },
       },
 
-sidebar: [
-  {
-    label: 'U1 · Fundamentos de redes',
-    translations: { en: 'U1 · Network Fundamentals' },
-    items: [
-      {
-        // Pon el nombre EXACTO de la carpeta que contiene los temas dentro de es/ y en/
-        // OJO: SIN "es/" delante. Si la carpeta se llama U01, pon 'U01'
-        autogenerate: { directory: 'U01' },
-      },
-    ],
-  },
-],
+      sidebar: [
+        {
+          label: 'U1 · Fundamentos de redes (ES)',
+          collapsed: false,
+          items: [
+            {
+              label: '1 · Comunicación y red de datos',
+              link: '/ASIR-Redes/es/u01/u0101comunicacion_y_red_de_datos_es/',
+            },
+            {
+              label: '2 · Componentes y primera representación',
+              link: '/ASIR-Redes/es/u01/u0102componentes_y_primera_representacion_es/',
+            },
+            {
+              label: '3 · Tipos de red y arquitecturas de servicio',
+              link: '/ASIR-Redes/es/u01/u0103tipos_de_red_y_arquitecturas_de_servicio_es/',
+            },
+          ],
+        },
+        {
+          label: 'U1 · Network Fundamentals (EN)',
+          collapsed: false,
+          items: [
+            {
+              label: '1 · Communication and data networks',
+              link: '/ASIR-Redes/en/u01/u0101communication_and_data_networks_en/',
+            },
+            {
+              label: '2 · Components and first network representation',
+              link: '/ASIR-Redes/en/u01/u0102components_and_first_network_representation_en/',
+            },
+            {
+              label: '3 · Network types and service architectures',
+              link: '/ASIR-Redes/en/u01/u0103network_types_and_service_architectures_en/',
+            },
+          ],
+        },
+      ],
 
       tableOfContents: false,
 
