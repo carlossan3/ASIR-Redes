d12a58c71235a4699d2f07c8c1149fb37458c270
Carlos <carlos.sanchez3@murciaeduca.es>
2026-09-17 18:16:20 +0200

    Astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -37,7 +37,7 @@ export default defineConfig({
         {
           label: 'Inicio',
           translations: { en: 'Home' },
-          slug: 'es',
+          slug: '/es/',
         },
         {
           label: 'U1 · Fundamentos de redes',
